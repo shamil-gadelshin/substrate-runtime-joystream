f90f5ce3015eadacd931681e52fb1db709a6a14d
Shamil Gadelshin <shamilgadelshin@gmail.com>
2021-04-08 11:56:50 +0300

    Add membership API.

--- storage-node-v2/src/api/controllers/publicApi.ts
+++ storage-node-v2/src/api/controllers/publicApi.ts
@@ -1,6 +1,9 @@
 import * as express from 'express'
  
-export function upload(_: express.Request, res: express.Response): void {
+export function upload(req: express.Request, res: express.Response): void {
+  console.log(req.body);
+  console.log(req.files);
+  
   res.json({
       file: "received"
   });
--- storage-node-v2/src/app.ts
+++ storage-node-v2/src/app.ts
@@ -1,5 +1,27 @@
 import {createServer} from './api/server'
 
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import { types } from '@joystream/types/'
+
+// Construct
+const wsProvider = new WsProvider('ws://localhost:9944');
+ApiPromise.create({ provider: wsProvider, types }).then(api => {
+  console.log(api.genesisHash.toHex());
+  const memberId = 0
+  
+  api.query.members.membershipById(memberId).then(
+    profile => {
+      console.log(profile)
+    }
+  ).catch(err => {
+    console.error(`Error: ${err}`)
+  })
+})
+.catch(err => {
+  console.error(`Error: ${err}`)
+})
+
+// Do something
 createServer()
   .then(server => {
     server.listen(4000, () => {
